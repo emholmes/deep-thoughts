c26c599a995d4d8940bf20ed8b68415d56db51f0
Erin Holmes <e.holmes09@yahoo.com>
2021-09-21 15:56:39 -0500

    install apollo server, create Thought type def and resolver

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+// import ApolloServer
+const { ApolloServer } = require("apollo-server-express");
+
+//import typeDefs and resolvers
+const { typeDefs, resolvers } = require("./schemas");
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// create a new Apollo server and pass in our schema data
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // context: authMiddleware
+  });
+
+  // start Apollo server
+  await server.start();
+
+  // integrate Apollo server with Express app as middleware
+  server.applyMiddleware({ app });
+
+  // log where we can go test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+}
+
+// initialize Apollo server
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
