ce84f848b0ecf2ee01959bfea9ba887ff0b09b6b
Erin Holmes <e.holmes09@yahoo.com>
2021-09-27 22:51:09 -0500

    update header to show links based on user logged in or not

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import AuthService from "../../utils/auth";
 
 const Header = () => {
+  const logout = e => {
+    e.preventDefault();
+    AuthService.logout();
+  };
+
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
@@ -10,8 +16,17 @@ const Header = () => {
         </Link>
 
         <nav className="text-center">
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
+          {AuthService.loggedIn() ? (
+            <>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>Logout</a>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </>
+          )}
         </nav>
       </div>
     </header>
